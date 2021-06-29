122e24421cbf071d06675e7fa422e18f229968b0
ritulsingh <ritulsingh00@gmail.com>
2021-06-30 02:26:38 +0530

    Some Changes

--- try.py
+++ try.py
@@ -1,3 +1,4 @@
-n = int(input()) #taking user input from keyboard
-for i in range(1, n+1): # running loop betwen the 1 to n + 1 
-    print(i, end='') # print the number
+string = input()
+print(string)
+eval("string")
+print
